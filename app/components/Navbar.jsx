4d6b8e0b36d9a646ac3a8a7095baf54215a1cf56
Montrail Cooper <Way2Automate@Montrails-MacBook-Pro.local>
2017-04-23 15:10:28 -0700

    Style Nav bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,18 +1,44 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router')
 
-var Navbar = () => {
-  return (
-    <div>
-      <h2>Navigation Component</h2>
-      <div>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      </div>
+var Navbar = React.createClass({
+  onSearch: function (e) {
+    e.preventDefault();
+    alert('Not yes wired up');
+  },
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">REACT WEATHER APP</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather" />
+              </li>
+            </ul>
+          </form>
+        </div>
 
-    </div>
-  );
-}
+      </div>
+    );
+  }
+});
 
 module.exports = Navbar;
